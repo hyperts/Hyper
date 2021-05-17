f2dd52de1bd3ed28a987b2ae93feef3413ebb367
nodge <matheus.silva3@ftc.edu.br>
2021-05-17 20:42:50 -0300

    feat: render statis and dynamic pages on settings window

--- src/render/windows/settings.tsx
+++ src/render/windows/settings.tsx
@@ -6,33 +6,42 @@ import '../style/index.css';
 import Titlebar from '../components/settings/titlebar';
 import Sidebar from '../components/settings/sidebar/sidebar';
 
-// import About from '../components/settings/pages/about';
-// import Themes from '../components/settings/pages/themes';
-// import Widgets from '../components/settings/pages/widgets';
-// import Dynamic from '../components/settings/pages/default';
+import About from '../components/settings/pages/about';
+import Themes from '../components/settings/pages/themes';
+import Widgets from '../components/settings/pages/widgets';
+import Dynamic from '../components/settings/pages/default';
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
 function App() {
-    const [active, SetActive] = useState('About');
+    const [active, SetActive] = useState('hyper.about');
 
-    function pageChange(page: string) {
-        console.log("Changed page")
-        SetActive(page)
+    function settingChanged() {
+
     }
 
-    // function renderItems() {
+    function pageChange(path: string) {
+        SetActive(path)
+    }
 
-    // }
+    function renderPage() {
+        switch (active) {
+            case 'hyper.about':
+                return <About />
+                break;
+            case 'hyper.themes':
+                return <Themes />
+            case 'hyper.about.widget':
+                return <Widgets />
+            default:
+                return <Dynamic path={active} change={settingChanged}/>
+        }
+    }
 
     return <>
         <Titlebar />
         <div className={`flex w-full h-screen settings-wrapper`}>
             <Sidebar active={active} onChange={pageChange}/>
-
-            <div className={`flex flex-col w-full h-full px-4 py-12 bg-bg rounded-r-md`} >
-                
-            </div>
         </div>
     </>
 }
