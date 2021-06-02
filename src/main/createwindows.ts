4279224b67d76899496c584d853c9529627de276
Nodge <matheus.silva3@ftc.edu.br>
2021-06-02 13:35:02 -0300

    Moved splashscreen to createWindows for clarity

--- src/main/createwindows.ts
+++ src/main/createwindows.ts
@@ -79,6 +79,33 @@ export function createWindows( windows: {[key: string]:BrowserWindow|null } ) {
     if (app.isPackaged) { windows.main.setMenuBarVisibility(false) }
 }
 
+export function createSplash(windows: {[key: string]:BrowserWindow}) {
+    console.log("Starting splash screen")
+
+    windows.splash = new BrowserWindow({
+        width: 600, 
+        height: 300, 
+        minimizable: false,
+        alwaysOnTop: true,
+        thickFrame: false,
+        frame: false,
+        transparent: true,
+        backgroundColor: '#00000000',
+        hasShadow: false,
+        resizable: false,
+        skipTaskbar: false,
+        focusable: true,
+        fullscreenable: false,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    })
+    ewc.setTransparentGradient(windows.splash, 0x0000000)
+    windows.splash.loadFile('./dist/loading.html')
+
+}
+
 export function createSettingsWindow(windows: {[key: string]:BrowserWindow|null }) {
     if (!windows.settings) {
         windows.settings = new BrowserWindow({
