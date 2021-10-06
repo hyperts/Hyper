71e0cb532984646ba4a48ae7235be6ae3998e388
Nodge <wbteuz@gmail.com>
2021-10-06 08:13:56 -0300

    test: testing pipe server

--- src/main.ts
+++ src/main.ts
@@ -45,3 +45,48 @@ app.on('ready', ()=>{
     }, 2000);
     
 })
+
+
+var net = require('net');
+
+var PIPE_NAME = "hyper";
+var PIPE_PATH = "\\\\.\\pipe\\" + PIPE_NAME;
+
+var server = net.createServer(function(stream: any) {
+    console.log('Server :: Connection')
+
+    stream.on('data', function(c: any) {
+        console.log('Server :: data:', c.toString());
+    });
+
+    stream.on('end', function() {
+        console.log('Server :: ended')
+        server.close();
+    });
+
+    stream.write(':: soft ::');
+});
+
+server.on('close',function(){
+    console.log('Server :: closed');
+})
+
+server.listen(PIPE_PATH,function(){
+    console.log('Server :: listening');
+})
+
+
+
+
+var client = net.connect(PIPE_PATH, function() {
+    console.log('Client :: connected');
+})
+
+client.on('data', function(data: any) {
+    console.log('Client: data:', data.toString());
+    client.end('OK');
+});
+
+client.on('end', function() {
+    console.log('Client :: ended');
+})
