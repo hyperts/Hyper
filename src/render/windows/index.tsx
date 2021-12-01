301432687a41ba698f6058ede9c4bfdea21a08f3
Nodge <matheus.silva3@ftc.edu.br>
2021-12-01 03:46:40 -0300

    feat: renders widget as react component

--- src/render/windows/index.tsx
+++ src/render/windows/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom"
 
 import '../style/index.css';
 
+//@ts-ignore
 import {loadWidgets, loadTheme} from '../utils'
 
 // import { openSettings } from '../ipc';
@@ -13,14 +14,27 @@ ReactDOM.render(<App />, document.getElementById('root'))
 
 
 function App() {
+
+    const loadedWidgets = loadWidgets()
     
     useEffect(() => {
-        loadWidgets()
+        loadedWidgets.map( widget =>{
+            widget.styles?.forEach( style => {
+                const head = document.querySelector('head')
+                const directory = widget.file.split('\\widgets\\')
+
+                if (head) { head.innerHTML += `<link rel="stylesheet" href="widgets://${directory[directory.length -1].split('\\')[0]}/${style}" type="text/css"/>`; }
+            })
+        })
         loadTheme()
     }, [])
 
 
     return <div id="hyperbar" className="flex flex-row">
-
+        {
+            loadedWidgets.map(widget =>{
+                return widget.default()
+            })
+        }
     </div>
 }
