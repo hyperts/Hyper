83ca339de9bed85ea48fe980f1a0ba4d16748fa8
Nodge <matheus.silva3@ftc.edu.br>
2021-12-15 04:49:41 -0300

    feat: checkdir now applied default modules

--- src/main/checkdir.ts
+++ src/main/checkdir.ts
@@ -1,6 +1,8 @@
 import { homedir } from 'os';
 import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
 import log from 'electron-log'
+import Zip from 'adm-zip'
+
 const homePath : String = homedir()
 const logger = log.scope('DIRECTORY')
 
@@ -13,7 +15,8 @@ if ( existsSync(`${homePath}\\.hyperbar`) ) {
     mkdirSync(`${homePath}\\.hyperbar`)
     mkdirSync(`${homePath}\\.hyperbar\\widgets`)
     mkdirSync(`${homePath}\\.hyperbar\\themes`)
-    firstRun = true
+    const zipFile = new Zip(`${__dirname}\\assets\\defaultmodules.zip`)
+    zipFile.extractAllTo(`${homePath}\\.hyperbar`, true)
 }
 
 if (firstRun || !existsSync(`${homePath}\\.hyperbar\\config.yaml`)) {
---
+++ src/render/components/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default loading;
